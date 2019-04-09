d0ae464cc2a0e796ac079ead5c4ccc4c23366b10
tslight <tobyslight@gmail.com>
2019-04-09 21:05:26 +0200

    recursive binary search

--- selection_sort.py
+++ selection_sort.py
@@ -16,9 +16,14 @@ def selection_sort(arr):
     return new_arr
 
 
-arr = input("Enter a series of numbers separated by spaces: ")
-arr = arr.split()
-arr = [int(e) for e in arr]
-print("Original array: {}".format(arr))
-sorted_arr = selection_sort(arr)
-print("Sorted array: {}".format(sorted_arr))
+def main():
+    arr = input("Enter a series of numbers separated by spaces: ")
+    arr = arr.split()
+    arr = [int(e) for e in arr]
+    print("Original array: {}".format(arr))
+    sorted_arr = selection_sort(arr)
+    print("Sorted array: {}".format(sorted_arr))
+
+
+if __name__ == '__main__':
+    main()
