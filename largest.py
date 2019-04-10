de8facefe73d9999842f75339bc94f0acb4abc12
tslight <tobyslight@gmail.com>
2019-04-10 20:51:38 +0200

    add quick sort and get largest

--- largest.py
+++ largest.py
@@ -3,11 +3,11 @@ from sys import argv
 from columns import prtcols
 
 
-def count(mylist):
-    if mylist == []:
-        return 0
-    else:
-        return 1 + count(mylist[1:])
+def get_largest(mylist):
+    if len(mylist) == 2:
+        return mylist[0] if mylist[0] > mylist[1] else mylist[1]
+    rest = get_largest(mylist[1:])
+    return mylist[0] if mylist[0] > rest else rest
 
 
 def main():
@@ -19,9 +19,10 @@ def main():
 
     mylist = [randint(1, (size * 100)) for i in range(0, size)]
     print("Number of elements: {}".format(len(mylist)))
-    number = count(mylist)
-    prtcols(mylist)
-    print("Number of elements: {}".format(number))
+    mylist = sorted(mylist, reverse=True)
+    largest = get_largest(mylist)
+    prtcols(sorted(mylist))
+    print("Largest element: {}".format(largest))
 
 
 if __name__ == '__main__':
--- sort.py
+++ sort.py
@@ -8,13 +8,16 @@ def get_args():
     parser = ArgumentParser(
         description='Sort a list using a variety of algorithms. ' +
         'Defaults to creating a list of 10 thousand elements, randomly, ' +
-        'from a pool of integers between 0 and 1 million.'
+        'from a pool of integers between 0 and 1 million and sorting them ' +
+        'with a custom quick sort algorithm.'
     )
-    group = parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group()
     group.add_argument('-s', '--selection', action='store_true',
                        help="Use selection sort.")
     group.add_argument('-q', '--quick', action='store_true',
                        help="Use quick sort.")
+    group.add_argument('-p', '--python', action='store_true',
+                       help="Use Python's built in sort.")
     parser.add_argument("-m", "--max", type=int, default='1_000_000',
                         help="Maximum number in list.")
     parser.add_argument("-S", "--size", type=int, default='10_000',
@@ -24,8 +27,14 @@ def get_args():
     return parser.parse_args()
 
 
-def quick():
-    pass
+def quick(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quick(less) + [pivot] + quick(greater)
 
 
 def find_smallest(arr):
@@ -49,16 +58,22 @@ def selection(arr):
 def mysort():
     args = get_args()
     mylist = [randint(0, args.max) for i in range(0, args.size)]
+    mylist = sorted(mylist, reverse=True)  # make as hard as possible..
 
     if args.selection:
         sorted_arr = selection(mylist)
         sort_type = "Selection Sort"
+    elif args.python:
+        sorted_arr = sorted(mylist)
+        sort_type = "Python Sort"
     else:
         sorted_arr = quick(mylist)
         sort_type = "Quick Sort"
 
     if args.verbose:
-        print("\nSorted array:")
+        print("\nOriginal array:\n")
+        prtcols(mylist)
+        print("\nSorted array:\n")
         prtcols(sorted_arr)
 
     print(
