6ec1ba89d1677538ca62a1ae0ad8a9b9f1304a20
tslight <tobyslight@gmail.com>
2019-04-21 17:25:09 +0100

    pretty confused!

--- dijkstra.py
+++ dijkstra.py
@@ -21,3 +21,27 @@ parents['b'] = 'start'
 parents['fin'] = None
 
 processed = []
+
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+        processed.append(node)
+        node = find_lowest_cost_node(costs)
